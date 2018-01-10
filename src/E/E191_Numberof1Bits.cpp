f52faa059bd68b8e5c4228817c8c166d004c396f
Andrii Cherniak <theandriicherniak@gmail.com>
2018-01-09 16:56:51 -0800

    Number of 1 Bits via bitset

--- src/E/E191_Numberof1Bits.cpp
+++ src/E/E191_Numberof1Bits.cpp
@@ -1,15 +1,12 @@
 //
 // Created by Andrii Cherniak on 1/9/18.
 //
+#include <bitset>
 
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-        int counter = 0;
-        while (n > 0){
-            n = n & (n - 1);
-            counter ++;
-        }
-        return counter;
+        std::bitset<32> bs(n);
+        return bs.count();
     }
 };
