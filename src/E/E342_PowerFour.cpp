1fe33564d8733650c7ebc162a70eb49d83594a25
Andrii Cherniak <theandriicherniak@gmail.com>
2018-01-10 10:53:23 -0800

    Power of Four: mod

---
+++ src/E/E342_PowerFour.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Andrii Cherniak on 1/9/18.
+//
+
+class Solution {
+public:
+    bool isPowerOfFour(int num) {
+        return num > 0 && (num & (num - 1)) == 0 && (num - 1) % 3 == 0;
+    }
+};
