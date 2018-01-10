76cf08ea259b7e459b471698f3e80dfde26a7b69
Andrii Cherniak <theandriicherniak@gmail.com>
2018-01-10 11:04:05 -0800

    Power of Four: bitmask pattern

--- src/E/E342_PowerFour.cpp
+++ src/E/E342_PowerFour.cpp
@@ -5,6 +5,7 @@
 class Solution {
 public:
     bool isPowerOfFour(int num) {
-        return num > 0 && (num & (num - 1)) == 0 && (num - 1) % 3 == 0;
+        unsigned int mask = 0b10101010101010101010101010101010;
+        return num > 0 && (num & (num - 1)) == 0 && (num & mask) == 0;
     }
 };
