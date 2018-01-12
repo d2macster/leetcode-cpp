c7f223144c3eaadf15bf48e496c6cf29c9aee554
Andrii Cherniak <theandriicherniak@gmail.com>
2018-01-11 18:03:14 -0800

    Bitwise AND of Numbers Range: speedup

--- src/M/M201_BitwiseAND.cpp
+++ src/M/M201_BitwiseAND.cpp
@@ -2,20 +2,18 @@
 // Created by Andrii Cherniak on 1/11/18.
 //
 
-#include <iostream>
-
 class Solution {
 public:
     int rangeBitwiseAnd(int m, int n) {
-        int result = n;
-        unsigned int mask = 0;
+        int result = n & m;
+        unsigned int mask = ~1;
         for (int i = 0; i < 32; i++){
-            mask = ~(1 << i);
             if ((n & mask) >= m){
                 result &= mask;
             }
+            mask = (mask << 1) | 1;
+
         }
-        result &= m;
         return result;
     }
 };
