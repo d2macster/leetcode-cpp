836d50429f2f9b2b62a82e881183b893f5d2ea05
Andrii Cherniak <theandriicherniak@gmail.com>
2018-01-15 15:28:20 -0800

    Word Break: speedup

--- src/M/M139_WordBreak.cpp
+++ src/M/M139_WordBreak.cpp
@@ -21,10 +21,13 @@ public:
         for (int i = 0; i < L; i++) dp[i] = false;
 
         for (int i = 0; i < L; i++) {
-            for (int j = 1; j <= i; j++) {
-                if (dp[j-1] && ws.find(s.substr(j, i - j + 1)) != ws.end()) dp[i] = true;
+            for (int j = i; j >= 1; j--) {
+                if (dp[j-1] && ws.find(s.substr(j, i - j + 1)) != ws.end()) {
+                    dp[i] = true;
+                    break;
+                }
             }
-            if (ws.find(s.substr(0, i + 1)) != ws.end()) dp[i] = true;
+            if (!dp[i] && ws.find(s.substr(0, i + 1)) != ws.end()) dp[i] = true;
 
         }
 
