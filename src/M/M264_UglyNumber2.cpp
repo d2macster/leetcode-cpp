02fea7251d020ed12551af47104424c76aed35a9
Andrii Cherniak <theandriicherniak@gmail.com>
2018-01-17 13:46:38 -0800

    Ugly Number II via lazy expansion

--- src/M/M264_UglyNumber2.cpp
+++ src/M/M264_UglyNumber2.cpp
@@ -1,52 +1,23 @@
 //
 // Created by Andrii Cherniak on 1/17/18.
 //
-#include <list>
-#include <iostream>
-#include <unordered_set>
-
-using std::list;
-using std::unordered_set;
-
 class Solution {
 public:
     int nthUglyNumber(int n) {
         if (n == 1)
             return 1;
-        unordered_set<int> s;
-        int count = 1;
-        int v = 1;
-        list<int> l2;
-        list<int> l3;
-        list<int> l5;
 
-        list<int> *p;
-
-        l2.push_back(2);
-        l3.push_back(3);
-        l5.push_back(5);
-
-        while (count < n) {
-            p = &l2;
-            if (l3.front() < p->front()) p = &l3;
-            if (l5.front() < p->front()) p = &l5;
-            v = p->front();
-            p->pop_front();
-
-            if (s.find(v) == s.end()) {
-                s.insert(v);
-
-                l2.push_back(v * 2);
-                l3.push_back(v * 3);
-                l5.push_back(v * 5);
-
-                count++;
-            }
+        int dp[n];
+        dp[0] = 1;
+        int p2, p3, p5, v;
+        p2 = p3 = p5 = 0;
+        for (int i = 1; i < n; i++){
+            v = std::min(std::min(dp[p2] * 2, dp[p3] * 3), dp[p5] * 5);
+            if (v == dp[p2] * 2) p2 ++;
+            if (v == dp[p3] * 3) p3 ++;
+            if (v == dp[p5] * 5) p5 ++;
+            dp[i] = v;
         }
-
-
-        return v;
-
-
+        return dp[n-1];
     }
 };
