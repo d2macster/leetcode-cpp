dbafd9a89477ef380139ce0810b84a4b7db8bba6
Andrii Cherniak <theandriicherniak@gmail.com>
2018-01-18 15:27:25 -0800

    Remove Duplicates from Sorted Array

---
+++ src/E/E26_RemoveDuplicatesSortedArray.cpp
@@ -0,0 +1,23 @@
+//
+// Created by Andrii Cherniak on 1/18/18.
+//
+#include <vector>
+using std::vector;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int L = nums.size();
+        int Ldd = L;
+        if (L <= 1) return L;
+        int ptr = 0;
+        for (int i = 1; i < L; i++){
+            if (nums[ptr] == nums[i]){
+                Ldd --;
+            } else{
+                nums[++ptr] = nums[i];
+            }
+        }
+        return Ldd;
+    }
+};
