0df2745da5fa2c0ac49c01fa0ea4d59295b1fdee
Andrii Cherniak <theandriicherniak@gmail.com>
2018-01-18 14:29:49 -0800

    Merge Sorted Array : simplified

--- src/E/E88_MergeSortedArray.cpp
+++ src/E/E88_MergeSortedArray.cpp
@@ -12,15 +12,7 @@ public:
             return;
         int i = n + m - 1;
         int p1 = m - 1, p2 = n - 1;
-        while (p1 >= 0 && p2 >= 0) {
-            if (nums1[p1] > nums2[p2]) {
-                nums1[i] = nums1[p1];
-                nums1[p1--] = 0;
-            } else
-                nums1[i] = nums2[p2--];
-            i--;
-        }
         while (p2 >= 0)
-            nums1[i--] = nums2[p2--];
+            nums1[i--] = (p1 >= 0) && (nums1[p1] > nums2[p2]) ? nums1[p1--] : nums2[p2--];
     }
 };
