141eb33d72fcde73125fea883ffb7110d5915072
Andrii Cherniak <theandriicherniak@gmail.com>
2018-01-18 12:14:03 -0800

    Best Time to Buy and Sell Stock II via greedy

--- src/E/E122_BuySellStock.cpp
+++ src/E/E122_BuySellStock.cpp
@@ -11,13 +11,11 @@ public:
         int L = prices.size();
         if (L <= 1)
             return 0;
-        int pb0 = -prices[0], pb1, ps0 = 0, ps1;
+        int profit = 0;
         for (int i = 1; i < L; i++){
-            pb1 = std::max(pb0, ps0 - prices[i]);
-            ps1 = std::max(ps0, pb0 + prices[i]);
-            pb0 = pb1;
-            ps0 = ps1;
+            if (prices[i] > prices[i-1])
+                profit += prices[i] - prices[i-1];
         }
-        return std::max(pb0, ps0);
+        return profit;
     }
 };
