620696862c4fc66b98963a48f7e501fdaa7ec851
Andrii Cherniak <theandriicherniak@gmail.com>
2018-02-02 10:24:40 -0800

    Longest Substring Without Repeating Characters via array

--- src/M/M3_LongestSubstringWithoutRepeatingCharacters.cpp
+++ src/M/M3_LongestSubstringWithoutRepeatingCharacters.cpp
@@ -2,11 +2,7 @@
 // Created by Andrii Cherniak on 2/1/18.
 //
 #include <string>
-#include <unordered_map>
-#include <iostream>
-
 using std::string;
-using std::unordered_map;
 
 class Solution {
 public:
@@ -14,8 +10,8 @@ public:
         int L = s.length();
         if (L <= 1)
             return L;
-
-        unordered_map<char , int> m;
+        int m[256];
+        for (int i = 0; i < 256; i++) m[i] = -1;
 
         int result = 1;
         int l = 0, r = 1;
@@ -23,11 +19,11 @@ public:
         char ch;
         while (r < L){
             ch = s[r];
-            if (m.find(ch) != m.end()){
+            if (m[ch] != -1){
                 int i = l;
                 l = m[ch] + 1;
                 for (; i <l; i++)
-                    m.erase(s[i]);
+                    m[s[i]] = -1;
             }
             result = std::max(result, r - l + 1);
             m[ch] = r;
