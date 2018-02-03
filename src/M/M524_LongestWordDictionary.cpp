690b20d1bf31b6eb1132d39a57e8d9d2759e23cb
Andrii Cherniak <theandriicherniak@gmail.com>
2018-02-02 17:53:46 -0800

    Longest Word in Dictionary through Deleting

--- src/M/M524_LongestWordDictionary.cpp
+++ src/M/M524_LongestWordDictionary.cpp
@@ -1,4 +1,30 @@
 //
 // Created by Andrii Cherniak on 2/2/18.
 //
+#include <string>
+#include <vector>
+using std::vector;
+using std::string;
 
+class Solution {
+public:
+    string findLongestWord(string s, vector<string>& d) {
+        int Ls = s.length();
+        int Lw, ps, pw;
+        string result = "";
+
+        for (string w : d){
+            Lw = w.length();
+            ps = 0;
+            pw = 0;
+            while (ps < Ls && pw < Lw)
+                if (s[ps++] == w[pw]) pw++;
+            if (pw == Lw){
+                if (pw > result.length()) result = w;
+                else if (pw == result.length()) result = (result < w)? result: w;
+
+            }
+        }
+        return result;
+    }
+};
