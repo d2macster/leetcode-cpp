15954e86658e22271ae45c9b8c0ca39becc3a89e
Andrii Cherniak <theandriicherniak@gmail.com>
2018-02-02 18:40:13 -0800

    Island Perimeter via boundaries of ones

--- src/E/4/E463_IslandPerimeter.cpp
+++ src/E/4/E463_IslandPerimeter.cpp
@@ -8,27 +8,20 @@ using std::vector;
 class Solution {
 public:
     int islandPerimeter(vector<vector<int> > &grid) {
-        int perimeter = 0;
         int R = grid.size();
         if (R == 0) return 0;
         int C = grid[0].size();
         if (C == 0) return 0;
+        int ones = 0, neighbours = 0;
 
         for (int r = 0; r < R; r++)
             for (int c = 0; c < C; c++) {
                 if (grid[r][c] == 1) {
-
-                    if (r == 0) perimeter++;
-                    if (r == (R - 1)) perimeter++;
-                    if (r > 0 && grid[r - 1][c] == 0) perimeter++;
-                    if (r < (R - 1) && grid[r + 1][c] == 0) perimeter++;
-
-                    if (c == 0) perimeter++;
-                    if (c == (C - 1)) perimeter++;
-                    if (c > 0 && grid[r][c - 1] == 0) perimeter++;
-                    if (c < (C - 1) && grid[r][c + 1] == 0) perimeter++;
+                    ones++;
+                    if (r > 0 && grid[r-1][c] == 1) neighbours++;
+                    if (c > 0 && grid[r][c-1] == 1) neighbours++;
                 }
             }
-        return perimeter;
+        return 4*ones - 2 * neighbours;
     }
 };
