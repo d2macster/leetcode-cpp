ca964bf4e811f60481deb884eb38b9a705fcc662
Andrii Cherniak <theandriicherniak@gmail.com>
2018-02-02 16:17:34 -0800

    Sort Colors via count sort

--- src/M/M75_SortColors.cpp
+++ src/M/M75_SortColors.cpp
@@ -1,4 +1,16 @@
 //
 // Created by Andrii Cherniak on 2/2/18.
 //
+#include <vector>
+using std::vector;
 
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int counter[3] = {0, 0, 0};
+        for (const int n: nums) counter[n]++;
+        int id = 0;
+        for (int v = 0; v <= 2; v++)
+        for (int i = 0; i < counter[v]; i++)nums[id++] = v;
+    }
+};
