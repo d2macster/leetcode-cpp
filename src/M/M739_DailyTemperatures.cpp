cf517a4ac678216412490973cccbb372f1f3ea53
Andrii Cherniak <theandriicherniak@gmail.com>
2018-02-07 11:42:32 -0800

    Daily Temperatures via map iterator

--- src/M/M739_DailyTemperatures.cpp
+++ src/M/M739_DailyTemperatures.cpp
@@ -2,25 +2,28 @@
 // Created by Andrii Cherniak on 2/7/18.
 //
 #include <vector>
-#include <unordered_map>
+#include <map>
 
 using std::vector;
-using std::unordered_map;
+using std::map;
 
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int> &temperatures) {
         int L = temperatures.size();
         vector<int> answer = vector<int>(L, 0);
-        unordered_map<int, int> m(L);
+        map<int, int> m;
+        map<int, int>::iterator it;
         int w_id;
         for (int i = L - 1; i >= 0; i--) {
             w_id = L;
-            for (int t = temperatures[i] + 1; t <= 100; t++)
-                if (m.find(t) != m.end()) w_id = std::min(w_id, m[t]);
+            m[temperatures[i]] = i;
+
+            for (it = ++m.find(temperatures[i]); it != m.end(); it++)
+                w_id = std::min(w_id, it->second);
+
             answer[i] = (w_id == L)? 0: w_id - i;
 
-            m[temperatures[i]] = i;
         }
 
         return answer;
