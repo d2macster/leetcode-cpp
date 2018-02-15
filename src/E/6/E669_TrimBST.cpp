4c548237c138a554215cc83a728b97e3150a0bd3
Andrii Cherniak <theandriicherniak@gmail.com>
2018-02-15 09:39:37 -0800

    Trim a Binary Search Tree

---
+++ src/E/6/E669_TrimBST.cpp
@@ -0,0 +1,26 @@
+//
+// Created by Andrii Cherniak on 2/15/18.
+//
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    TreeNode *trimBST(TreeNode *root, int L, int R) {
+        if (!root) return NULL;
+
+        if (root->val > R) return trimBST(root->left, L, R);
+        if (root->val < L) return trimBST(root->right, L, R);
+
+        root->left = trimBST(root->left, L, R);
+        root->right = trimBST(root->right, L, R);
+        return root;
+    }
+};
