af3ea21f6ddcb319eaaecc49d1eda6234d59f03b
Andrii Cherniak <theandriicherniak@gmail.com>
2018-03-11 16:45:24 -0700

    Verify Preorder Sequence in Binary Search Tree: via stack

--- src/M/2/M255_VerifyPreorderSequenceBST.cpp
+++ src/M/2/M255_VerifyPreorderSequenceBST.cpp
@@ -2,29 +2,33 @@
 // Created by Andrii Cherniak on 3/11/18.
 //
 #include <vector>
-#include <iostream>
+#include <stack>
+#include <limits>
 
 using std::vector;
 using namespace std;
 
 class Solution {
-    bool check_helper(vector<int> &preorder, int l, int r) {
-        if (r - l <= 0) return true;
-        int root = preorder[l];
-        int less_id = l;
-        while (less_id + 1 <= r && preorder[less_id + 1] < root) less_id++;
-        for (int i = less_id+1; i <= r; i++) if (preorder[i] < root) return false;
-
-        if (!check_helper(preorder, l+1, less_id))
-            return false;
-        if (!check_helper(preorder, less_id + 1, r)) return false;
-        return true;
-
-    }
-
 public:
     bool verifyPreorder(vector<int> &preorder) {
-        int L = preorder.size();
-        return check_helper(preorder, 0, L - 1);
+        if (preorder.size() <= 1)
+            return true;
+        stack<int> s;
+        int parent = INT_MIN;
+        for (int v: preorder) {
+            if (s.empty() || s.top() > v) {
+                if (v < parent)
+                    return false;
+                s.push(v);
+            }
+            else {
+                while (!s.empty() && s.top() < v) {
+                    parent = s.top();
+                    s.pop();
+                }
+                s.push(v);
+            }
+        }
+        return true;
     }
 };
