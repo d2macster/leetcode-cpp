134290693914010effaa313e95f73dc9afb3123e
Andrii Cherniak <theandriicherniak@gmail.com>
2018-03-12 16:35:16 -0700

    Add Binary

--- src/E/6/E67_AddBinary.cpp
+++ src/E/6/E67_AddBinary.cpp
@@ -10,28 +10,18 @@ public:
     string addBinary(string a, string b) {
         int La = a.size();
         int Lb = b.size();
-        if (!La) return b;
-        if (!Lb) return a;
-        if (Lb > La) {
-            std::swap(La, Lb);
-            std::swap(a, b);
-        }
         int overflow = 0;
+        string result = "";
 
-        int pa = La - 1, pb = Lb - 1;
-        while (pa >= 0 || pb >= 0) {
+        for (int pa = La - 1, pb = Lb - 1; pa >= 0 || pb >= 0 || overflow; pa--, pb--) {
             int va = (pa >= 0) ? a[pa] - '0' : 0;
             int vb = (pb >= 0) ? b[pb] - '0' : 0;
             int v = va + vb + overflow;
-            cout << v << endl;
             overflow = ((v & 2) >> 1);
             v &= 1;
-            a[pa] = (v) ? '1' : '0';
-            pa--;
-            pb--;
+            result = (char) (v + '0') + result;
         }
-        if (overflow > 0)
-            a = '1' + a;
-        return a;
+
+        return result;
     }
 };
