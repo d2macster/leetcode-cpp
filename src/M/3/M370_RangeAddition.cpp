2b4ace62de80ecc6f8cdc690887a79d17b780e83
Andrii Cherniak <theandriicherniak@gmail.com>
2018-03-12 17:13:43 -0700

    Range Addition: optimized

--- src/M/3/M370_RangeAddition.cpp
+++ src/M/3/M370_RangeAddition.cpp
@@ -2,19 +2,26 @@
 // Created by Andrii Cherniak on 3/12/18.
 //
 #include <vector>
+
 using std::vector;
 
 class Solution {
 public:
-    vector<int> getModifiedArray(int length, vector<vector<int> >& updates) {
-        vector<int > result = vector<int >(length, 0);
-        for (auto u: updates){
+    vector<int> getModifiedArray(int length, vector<vector<int> > &updates) {
+        vector<int> result = vector<int>(length, 0);
+        for (auto u: updates) {
             int begin = u[0];
             int end = u[1];
             int increment = u[2];
-            for (int i = begin; i <= end; i++) result[i] += increment;
+            result[begin] += increment;
+            if (end + 1 < length)
+                result[end + 1] -= increment;
+        }
+        int cummulative = 0;
+        for (int i = 0; i < length; i++) {
+            cummulative += result[i];
+            result[i] = cummulative;
         }
         return result;
-
     }
 };
