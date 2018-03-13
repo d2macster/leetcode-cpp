786ee94429cc6c71661fce2cb35d4f82cbbc2508
Andrii Cherniak <theandriicherniak@gmail.com>
2018-03-13 14:29:59 -0700

    Combination Sum speedup via sorting

--- src/M/3/M39_CombinationSum.cpp
+++ src/M/3/M39_CombinationSum.cpp
@@ -2,24 +2,27 @@
 // Created by Andrii Cherniak on 3/13/18.
 //
 #include <vector>
+#include <algorithm>
 #include <unordered_set>
 #include <iostream>
 
 using namespace std;
 
 class Solution {
-    void helper(vector<int> &candidates, vector<int> seq, vector<vector<int> > &result, int l_id, int r_id,
+    void helper(vector<int> &candidates, vector<int> seq, vector<vector<int> > &result, int c_id,
                 int target) {
 
-        if (target < 0) return;
-        else if (target == 0) {
-            result.push_back(seq);
-            return;
-        }
-        for (int i = l_id; i <= r_id; i++) {
-            vector<int> seq_next = seq;
-            seq_next.push_back(candidates[i]);
-            helper(candidates, seq_next, result, i, r_id, target - candidates[i]);
+        if (target < 0 || c_id >= candidates.size() || candidates[c_id] > target) return;
+
+        helper(candidates, seq, result, c_id + 1, target);
+        while (target - candidates[c_id] >= 0) {
+            target -= candidates[c_id];
+            seq.push_back(candidates[c_id]);
+            if (target == 0) {
+                result.push_back(seq);
+                return;
+            }
+            helper(candidates, seq, result, c_id + 1, target);
         }
     }
 
@@ -27,8 +30,10 @@ public:
     vector<vector<int> > combinationSum(vector<int> &candidates, int target) {
         int L = candidates.size();
         vector<vector<int> > result;
+        std::sort(candidates.begin(), candidates.end());
         vector<int> seq;
-        helper(candidates, seq, result, 0, L - 1, target);
+        helper(candidates, seq, result, 0, target);
+
         return result;
     }
 };
