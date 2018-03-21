9a83a130e123312c39401da2a25d782f10a39052
Andrii Cherniak <theandriicherniak@gmail.com>
2018-03-20 19:46:11 -0700

    calculator : refactoring

--- src/M/2/M227_BasicCalculator.cpp
+++ src/M/2/M227_BasicCalculator.cpp
@@ -6,30 +6,7 @@
 using namespace std;
 
 class Solution {
-public:
-    int calculate(string s) {
-        int v = 0, sum = 0, tmp = 0;
-        char op = '+';
-
-        for (char c: s) {
-            if (c >= '0' && c <= '9') v = v * 10 + (c - '0');
-            else if (c == '+' || c == '-' || c == '*' || c == '/') {
-                if (op == '-') {
-                    sum += tmp;
-                    tmp = -v;
-                }
-                if (op == '+') {
-                    sum += tmp;
-                    tmp = v;
-                }
-                if (op == '*') tmp *= v;
-                if (op == '/') tmp /= v;
-
-                op = c;
-                v = 0;
-            }
-        }
-
+    void helper(int &sum, int &tmp, int & v, char op) {
         if (op == '-') {
             sum += tmp;
             tmp = -v;
@@ -40,6 +17,24 @@ public:
         }
         if (op == '*') tmp *= v;
         if (op == '/') tmp /= v;
+    }
+
+public:
+    int calculate(string s) {
+        int v = 0, sum = 0, tmp = 0;
+        char op = '+';
+
+        for (char c: s) {
+            if (c >= '0' && c <= '9') v = v * 10 + (c - '0');
+            else if (c == '+' || c == '-' || c == '*' || c == '/') {
+                helper(sum, tmp, v, op);
+
+                op = c;
+                v = 0;
+            }
+        }
+
+        helper(sum, tmp, v, op);
 
         return sum + tmp;
     }
