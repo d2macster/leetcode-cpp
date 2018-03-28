fb01857f5c97605471ebc57970594bce079722b5
Andrii Cherniak <theandriicherniak@gmail.com>
2018-03-28 11:16:39 -0700

    Target Sum with forward backwards dp

--- src/M/4/M494_TargetSum.cpp
+++ src/M/4/M494_TargetSum.cpp
@@ -3,29 +3,54 @@
 //
 #include <vector>
 #include <unordered_map>
+#include <iostream>
+
 using namespace std;
 
 
 class Solution {
 public:
-    int findTargetSumWays(vector<int>& nums, int S) {
+    int findTargetSumWays(vector<int> &nums, int S) {
         int L = nums.size();
-        unordered_map<int , int> counter_prev;
-        unordered_map<int , int> counter_next;
+        unordered_map<int, int> counter_prev;
+        unordered_map<int, int> counter_forward;
+        unordered_map<int, int> counter_backwards;
+
 
         counter_prev[0] = 1;
+        counter_forward = counter_prev;
 
-        for (int n: nums){
-            counter_next.clear();
-            for (auto it: counter_prev){
-                counter_next[it.first + n] += it.second;
-                counter_next[it.first - n] += it.second;
+
+        for (int i = 0; i < L / 2; i++) {
+            counter_forward.clear();
+            for (auto it: counter_prev) {
+                counter_forward[it.first + nums[i]] += it.second;
+                counter_forward[it.first - nums[i]] += it.second;
             }
-            counter_prev = counter_next;
+            counter_prev = counter_forward;
+        }
+
+        counter_prev.clear();
+        counter_prev[0] = 1;
+        counter_backwards = counter_prev;
+
+        for (int i = L - 1; i >= L / 2; i--) {
+            counter_backwards.clear();
+            for (auto it: counter_prev) {
+                counter_backwards[it.first + nums[i]] += it.second;
+                counter_backwards[it.first - nums[i]] += it.second;
+            }
+            counter_prev = counter_backwards;
         }
 
 
-        return counter_prev[S];
+        int ways = 0;
+        for (auto it: counter_forward) {
+            int diff = S - it.first;
+            ways += it.second * counter_backwards[diff];
+        }
+
+        return ways;
 
     }
 };
