1c09d2a2db6ced9d86a3adad0744e9859af1fc18
Andrii Cherniak <theandriicherniak@gmail.com>
2018-03-28 10:52:42 -0700

    Target Sum with memoization

--- src/M/4/M494_TargetSum.cpp
+++ src/M/4/M494_TargetSum.cpp
@@ -2,29 +2,30 @@
 // Created by Andrii Cherniak on 3/28/18.
 //
 #include <vector>
+#include <unordered_map>
 using namespace std;
 
 
 class Solution {
-    int ways = 0;
-    void helper(vector<int>& nums, int S, int n_id, int cur_sum){
-        if (n_id == nums.size()) {
-            if (cur_sum == S) ways++;
-            return;
-        }
-        helper(nums, S, n_id + 1, cur_sum + nums[n_id]);
-        helper(nums, S, n_id + 1, cur_sum - nums[n_id]);
-
-    }
 public:
     int findTargetSumWays(vector<int>& nums, int S) {
-        int s = 0;
-        for (int n: nums) s += n;
-        if (abs(s) < abs(S)) return 0;
+        int L = nums.size();
+        unordered_map<int , int> counter_prev;
+        unordered_map<int , int> counter_next;
 
-        helper(nums, S, 0, 0);
+        counter_prev[0] = 1;
 
-        return ways;
+        for (int n: nums){
+            counter_next.clear();
+            for (auto it: counter_prev){
+                counter_next[it.first + n] += it.second;
+                counter_next[it.first - n] += it.second;
+            }
+            counter_prev = counter_next;
+        }
+
+
+        return counter_prev[S];
 
     }
 };
