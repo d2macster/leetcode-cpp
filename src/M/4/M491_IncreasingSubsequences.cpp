3328d461b5e994468ca0d05701012829a206df8b
Andrii Cherniak <theandriicherniak@gmail.com>
2018-03-28 17:48:43 -0700

    Increasing Subsequences: speedup wia dfs + set

--- src/M/4/M491_IncreasingSubsequences.cpp
+++ src/M/4/M491_IncreasingSubsequences.cpp
@@ -2,36 +2,30 @@
 // Created by Andrii Cherniak on 3/28/18.
 //
 #include <vector>
-#include <set>
+#include <unordered_set>
 
 using namespace std;
 
 class Solution {
+    void dfs(vector<int> &nums, vector<vector<int> > &result, vector<int> &sequence, int n_id) {
+        int Ls = sequence.size();
+        if (Ls >= 2) result.push_back(sequence);
+        unordered_set<int > seen_number;
+        for (int i = n_id; i < nums.size(); i++) {
+            if ((Ls == 0 || nums[i] >= sequence[Ls-1] ) && (seen_number.find(nums[i]) == seen_number.end())) {
+                sequence.push_back(nums[i]);
+                dfs(nums, result, sequence, i+1);
+                sequence.pop_back();
+                seen_number.insert(nums[i]);
+            }
+        }
+    }
+
 public:
     vector<vector<int> > findSubsequences(vector<int> &nums) {
-        set<vector<int> > candidates;
-        int L = nums.size();
-        for (int i = L - 1; i >= 0; i--) {
-            set<vector<int> > extra_candidates;
-            for (vector<int> c: candidates) {
-                if (c[0] >= nums[i]) {
-                    vector<int> copy = c;
-                    copy.insert(copy.begin(), nums[i]);
-                    extra_candidates.insert(copy);
-                }
-            }
-
-            vector<int> el = vector<int>(1, nums[i]);
-            candidates.insert(el);
-
-            for (vector<int> c: extra_candidates) candidates.insert(c);
-
-        }
         vector<vector<int> > result;
-        for (vector<int> c: candidates) {
-            if (c.size() > 1) result.push_back(c);
-        }
+        vector<int> sequence;
+        dfs(nums, result, sequence, 0);
         return result;
-
     }
 };
