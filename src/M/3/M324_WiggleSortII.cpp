0d2905bc0b9198eef6aefe012418f1128ec11738
Andrii Cherniak <theandriicherniak@gmail.com>
2018-04-05 13:11:41 -0700

    Wiggle Sort II: O(n) space, without reverse

--- src/M/3/M324_WiggleSortII.cpp
+++ src/M/3/M324_WiggleSortII.cpp
@@ -2,25 +2,20 @@
 // Created by Andrii Cherniak on 4/5/18.
 //
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 class Solution {
 public:
     void wiggleSort(vector<int> &nums) {
-        int L = nums.size();
-        if (L <= 1)
-            return;
         vector<int> copy = nums;
         sort(copy.begin(), copy.end());
-        int oddN = (L % 2 == 0) ? L / 2 : L / 2 + 1;
-        vector<int>::iterator it = copy.begin();
-        advance(it, oddN);
-        reverse(copy.begin(), it);
-
-        int l = 0, r = L-1;
+        int L = nums.size();
+        int oddN = (L % 2 == 0)?L/2:L/2+1;
+        int even_id = L-1;
+        int odd_id = oddN-1;
         for (int i = 0; i < L; i++)
-            nums[i] = (i % 2 == 0)?copy[l++]:copy[r--];
-
+            nums[i] = (i%2 == 0)?copy[odd_id--]:copy[even_id--];
     }
 };
