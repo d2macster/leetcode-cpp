626d6f7cda6e8df6dc2a14a5ce28493c5be70256
Andrii Cherniak <theandriicherniak@gmail.com>
2018-04-05 15:05:25 -0700

    Max Consecutive Ones II

--- src/M/4/M487_MaxConsecutiveOnesII.cpp
+++ src/M/4/M487_MaxConsecutiveOnesII.cpp
@@ -8,29 +8,17 @@ using namespace std;
 class Solution {
 public:
     int findMaxConsecutiveOnes(vector<int> &nums) {
-        int L = nums.size();
-        vector<int> forward_sum = vector<int>(L, 0);
-        vector<int> backwards_sum = vector<int>(L, 0);
-        int s = 0;
-        for (int i = 0; i < L; i++) {
-            s = (nums[i] == 0) ? 0 : s + nums[i];
-            forward_sum[i] = s;
-        }
-        s = 0;
-        for (int i = L - 1; i >= 0; i--) {
-            s = (nums[i] == 0) ? 0 : s + nums[i];
-            backwards_sum[i] = s;
+        int prev_sum = 0, cur_sum = 0;
+        int maxL = 0;
+        for (int n: nums) {
+            cur_sum++;
+            if (n == 0){
+                prev_sum = cur_sum;
+                cur_sum = 0;
+            }
+            maxL = max(maxL, cur_sum + prev_sum);
         }
 
-        int maxL = 0;
-        for (int i = 0; i < L; i++){
-            maxL = max(maxL, forward_sum[i]);
-            if (nums[i] == 0) {
-                int left = (i > 0) ? forward_sum[i - 1] : 0;
-                int right = (i < L - 1) ? backwards_sum[i + 1] : 0;
-                maxL = max(maxL, left + 1 + right);
-            }
-        }
 
         return maxL;
     }
