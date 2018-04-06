cc23bbea79f290d221f838b20fbea3a7fc9e0d6e
Andrii Cherniak <theandriicherniak@gmail.com>
2018-04-05 17:00:04 -0700

    Subarray Product Less Than K

--- src/M/7/M713_SubarrayProductLessK.cpp
+++ src/M/7/M713_SubarrayProductLessK.cpp
@@ -1,4 +1,32 @@
 //
 // Created by Andrii Cherniak on 4/5/18.
 //
+#include <vector>
+#include <iostream>
 
+using namespace std;
+
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int> &nums, int k) {
+        if (k == 0)
+            return 0;
+        int cnt = 0;
+        long kk = long(k);
+
+        int L = nums.size();
+        if (L == 0)
+            return 0;
+
+
+        int left = 0;
+        long p = 1L;
+        for (int right = 0; right < L && left < L; right++){
+            p *= nums[right];
+            while (p >= kk && left < L) p /= nums[left++];
+            cnt += right - left + 1;
+        }
+
+        return max(0, cnt);
+    }
+};
