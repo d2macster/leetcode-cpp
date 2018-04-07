9dabcd817d1eaf68db35b64f4f4a32973f9f8208
Andrii Cherniak <theandriicherniak@gmail.com>
2018-04-06 18:19:40 -0700

    Merge k Sorted Lists via priority queue

--- src/H/H23_MergeKSortedLists.cpp
+++ src/H/H23_MergeKSortedLists.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <vector>
+#include <queue>
 
 using namespace std;
 
@@ -12,46 +13,37 @@ struct ListNode {
 
     ListNode(int x) : val(x), next(NULL) {}
 };
+struct comparator{
+    bool operator()(ListNode * l1, ListNode * l2){
+        return (l1->val > l2->val);
+    }
+};
 
 class Solution {
-    ListNode *mergeTwo(ListNode *l1, ListNode *l2) {
-        ListNode *head = NULL;
-        ListNode *tail = NULL;
-        ListNode *next;
-        while (l1 != NULL || l2 != NULL){
-            if (l1 == NULL) {
-                next = l2;
-                l2 = l2->next;
-            } else if (l2 == NULL){
-                next = l1;
-                l1 = l1->next;
-            } else{
-                if (l1->val < l2->val){
-                    next = l1;
-                    l1 = l1->next;
-                } else{
-                    next = l2;
-                    l2 = l2->next;
-                }
-            }
-            if (head == NULL)
-                head = tail = next;
-            else{
-                tail->next = next;
-                tail = next;
-            }
-            next->next = NULL;
-        }
-        return head;
-    }
+
 
 public:
     ListNode *mergeKLists(vector<ListNode *> &lists) {
         int L = lists.size();
         if (L == 0) return NULL;
         if (L == 1) return lists[0];
-        ListNode *head = lists[0];
-        for (int i = 1; i < L; i++) head = mergeTwo(head, lists[i]);
+        ListNode *head = NULL;
+        ListNode * tail = NULL;
+        priority_queue<ListNode*, vector<ListNode*>, comparator> q;
+        for (int i = 0; i < L; i++)
+            if (lists[i] != NULL) q.push(lists[i]);
+        while (!q.empty()){
+            ListNode* n = q.top();
+            q.pop();
+            if (n->next != NULL) q.push(n->next);
+
+            if (head == NULL) head = tail = n;
+            else{
+                tail->next = n;
+                tail = n;
+            }
+            n->next = NULL;
+        }
         return head;
     }
 };
