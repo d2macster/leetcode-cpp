bd25839180712b3ffdee01f1bcb329f0469c0139
Andrii Cherniak <theandriicherniak@gmail.com>
2018-04-13 11:42:00 -0700

    Search a 2D Matrix II: via space pruning

--- src/M/2/M240_Search2DMatrixII.cpp
+++ src/M/2/M240_Search2DMatrixII.cpp
@@ -2,7 +2,6 @@
 // Created by Andrii Cherniak on 4/13/18.
 //
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
@@ -15,20 +14,11 @@ public:
         if (C == 0) return false;
 
         if (target < matrix[0][0] || target > matrix[R - 1][C - 1]) return false;
-        int lo = 0, hi = R - 1;
-        int row_begin = 0;
-        while (lo < hi) {
-            int mid = (lo + hi) / 2;
-            if (target == matrix[mid][0] || target == matrix[mid][C - 1]) return true;
-            else if (target > matrix[mid][C - 1]) lo = mid + 1;
-            else hi = mid - 1;
-        }
-        if (target >= matrix[lo][0]) row_begin = lo;
-        else row_begin = hi;
-        for (int r = row_begin; r < R; r++) {
-            if (target >= matrix[r][0] && target <= matrix[r][C - 1]) {
-                if (binary_search(matrix[r].begin(), matrix[r].end(), target)) return true;
-            }
+        int r = R - 1, c = 0;
+        while (r >= 0 && c < C){
+            if (matrix[r][c] == target) return true;
+            else if (target < matrix[r][c]) r--;
+            else c++;
         }
         return false;
     }
