4cea1278c5aebe2c83d4305fccd13bf225be49e1
Andrii Cherniak <theandriicherniak@gmail.com>
2018-04-30 18:22:16 -0700

    Continuous Subarray Sum

--- src/M/5/M523_ContinuousSubarraySum.cpp
+++ src/M/5/M523_ContinuousSubarraySum.cpp
@@ -2,32 +2,29 @@
 // Created by Andrii Cherniak on 4/30/18.
 //
 #include <vector>
-#include <unordered_set>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution {
 public:
     bool checkSubarraySum(vector<int> &nums, int k) {
-        unordered_set<int> s1, s2;
         int L = nums.size();
         if (L < 2)
             return false;
 
-        k = abs(k);
-        s1.insert(nums[0]);
-
-        for (int i = 1; i < L; i++) {
-            s2.clear();
-            for (int s: s1){
-                int next_v = s + nums[i];
-                if (k == 0 && next_v == 0) return true;
-                if (k != 0 && next_v % k == 0) return true;
-                s2.insert(next_v);
+        unordered_map<int, int> residuals;
+        residuals[0] = -1;
+        int s = 0;
+        for (int i = 0; i < L; i++){
+            s += nums[i];
+            if (k != 0) s %= k;
+            if (residuals.find(s) != residuals.end()){
+                if (i - residuals[s] > 1) return true;
             }
-            s2.insert(nums[i]);
-            s1 = s2;
+            else residuals[s] = i;
         }
+
 
         return false;
 
