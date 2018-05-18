21c070ed873c85528a3eb6f0a644becbb3b1ab6c
Andrii Cherniak <theandriicherniak@gmail.com>
2018-05-18 14:55:21 -0700

    bulb switcher

---
+++ src/M/3/M319_BulbSwitcher.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Andrii Cherniak on 5/18/18.
+//
+#include <cmath>
+using namespace std;
+
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        return sqrt(n);
+    }
+};
