67b1cc11b507e9fddb8bccb3934f79a2591aef4e
Andrii Cherniak <theandriicherniak@gmail.com>
2018-05-18 16:45:01 -0700

    Battleships in a Board: without deletion

--- src/M/4/M419_BattleshipsBoard.cpp
+++ src/M/4/M419_BattleshipsBoard.cpp
@@ -6,10 +6,6 @@
 using namespace std;
 
 class Solution {
-    void removeShip(vector<vector<char>> &board, int r, int c, int R, int C) {
-        for (int i = c; i < C && board[r][i] == 'X'; i++) board[r][i] = '.';
-        for (int i = r+1; i < R && board[i][c] == 'X'; i++) board[i][c] = '.';
-    }
 
 public:
     int countBattleships(vector<vector<char>> &board) {
@@ -20,8 +16,7 @@ public:
         for (int r = 0; r < R; r++) {
             for (int c = 0; c < C; c++) {
                 if (board[r][c] == 'X') {
-                    counter++;
-                    removeShip(board, r, c, R, C);
+                    if ((c == 0 || board[r][c - 1] == '.') && (r == 0 || board[r - 1][c] == '.')) counter++;
                 }
             }
         }
