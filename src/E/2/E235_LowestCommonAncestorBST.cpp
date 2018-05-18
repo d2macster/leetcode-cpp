a6cb8353ff69215701d7370f66dbea8bcc6ebaae
Andrii Cherniak <theandriicherniak@gmail.com>
2018-05-18 15:04:30 -0700

    Lowest Common Ancestor of a Binary Search Tree

--- src/E/2/E235_LowestCommonAncestorBST.cpp
+++ src/E/2/E235_LowestCommonAncestorBST.cpp
@@ -1,7 +1,3 @@
-//
-// Created by Andrii Cherniak on 2/14/18.
-//
-
 
 struct TreeNode {
     int val;
@@ -14,10 +10,9 @@ struct TreeNode {
 class Solution {
 public:
     TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q) {
-        if (!root || !p || !q) return NULL;
-        if ((p->val - root->val) * (q->val - root->val) > 0){
-            if (p->val < root->val) return lowestCommonAncestor(root->left, p, q);
-            else return lowestCommonAncestor(root->right, p, q);
-        }else return root;
+        if (!root) return NULL;
+        if (p->val < root->val && q->val < root->val) return lowestCommonAncestor(root->left, p, q);
+        if (p->val > root->val && q->val > root->val) return lowestCommonAncestor(root->right, p, q);
+        return root;
     }
 };
